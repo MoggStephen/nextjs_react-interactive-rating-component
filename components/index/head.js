c195c8484a91829c9fadaab78d1022c9ee54b2d6
MoggStephen <moggstephen66@gmail.com>
2023-04-27 17:30:17 +0100

    family fix?

--- components/index/head.js
+++ components/index/head.js
@@ -10,6 +10,7 @@ export default function Head(){
                 <meta name="description" content="App to practice react and next.js"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
+                <link href="https://fonts.googleapis.com/css?family=Overpass:400,700&display=swap" rel="stylesheet"/>
             </NextHead>
         </>
     )
