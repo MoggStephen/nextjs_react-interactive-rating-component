f7a202bf190c33b0d6f5234b3d00273eb0309095
MoggStephen <moggstephen66@gmail.com>
2023-04-28 01:13:01 +0100

    almost ready for fem

--- components/index/footer.js
+++ components/index/footer.js
@@ -2,7 +2,18 @@ export default function Footer(){
 
     return(
         <>
-                      
+            {/* LINKS FOOTER */}
+            <footer style={{position:'fixed', bottom:'0', width:'100%', textAlign:'center', zIndex:'500'}}>
+                <a style={{textDecoration:'none', zIndex:'500'}} target="_blank" href="https://github.com/MoggStephen/swiper-full-page-with-scroll">
+                    <img src="./footer/github-icon.svg" alt="Github icon" width="25"/>
+                </a>
+                <a style={{textDecoration:'none', zIndex:'500', margin: '0 5px 0 5px'}} target="_blank" href="https://www.frontendmentor.io/profile/MoggStephen">
+                    <img src="./footer/fem-icon.svg" alt="Frontend Master icon" width="25"/>
+                </a>
+                <a style={{textDecoration:'none', zIndex:'500'}}  target="_blank" href="https://www.linkedin.com/in/stephen-mogg-9467041bb/">
+                    <img src="./footer/linkedin-icon.svg" alt="LinkedIn icon" width="25"/>
+                </a>
+            </footer> 
         </>
     );
 }
